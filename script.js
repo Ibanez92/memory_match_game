6e6e3357125783f66c70ccddfb8ca54cf739ccbd
Javier Ibanez <Expertprep2015@gmail.com>
2023-05-17 23:22:04 -0400

    removed html elements from html and added them to my innerHTML function

--- script.js
+++ script.js
@@ -16,8 +16,18 @@ for (let i = 0; i < cardNumbers.length; i++) {
     newCard.textContent.createElement('div')
     
     newCard.innerHTML = `
-    <div class='flip-card-inner'>
-    <div class=''>
+    <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+          </div>
+          <div class="flip-card-back">
+            <h1>John Doe</h1>
+            <p>Architect & Engineer</p>
+            <p>We love that guy</p>
+          </div>
+        </div>
+      </div>
     `
     
 }
